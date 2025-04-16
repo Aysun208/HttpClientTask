2ebb3e3e182caaf3562f684e26be40d3488c45fc
Aysun208 <aysunmm-bb208@code.edu.az>
2025-04-17 00:46:28 +0400

    task

---
+++ HttpClientClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClient
+{
+    public class HttpClientClass
+    {
+        public static async Task GetData(List<string> urls)
+        {
+            HttpClientClass client = new HttpClientClass();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            foreach (string url in urls)
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("request send:" + url);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Request failed:{url} . Status code:{response.StatusCode}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error request {url} {ex.Message}");
+                }
+            }
+            stopwatch.Stop();
+            Console.WriteLine($"umumi vaxt: {stopwatch.Elapsed.TotalSeconds} saniyedir");
+        }
+        private async Task<HttpResponseMessage> GetAsync(string url)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HttpClient
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            List<string> urls = new List<string>()
+            {
+               "https://www.youtube.com",
+               "https://www.githup.com",
+               "https://www.google.com",
+               "https://www.apple.com",
+               "https://www.amazon.com"
+            };
+            HttpClientClass.GetData(urls).Wait();
+        }
+    }
+}
